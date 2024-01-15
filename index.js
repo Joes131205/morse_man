b741a3577535b31e68cad6ba413a9656ff0d6d5e
Joe Steven <stevenjoe1312@gmail.com>
2024-01-15 20:43:56 +0700

    continuing

---
+++ index.js
@@ -0,0 +1,76 @@
+const alphabetToMorse = {
+    a: ".-",
+    b: "-...",
+    c: "-.-.",
+    d: "-..",
+    e: ".",
+    f: "..-.",
+    g: "--.",
+    h: "....",
+    i: "..",
+    j: ".---",
+    k: "-.-",
+    l: ".-..",
+    m: "--",
+    n: "-.",
+    o: "---",
+    p: ".--.",
+    q: "--.-",
+    r: ".-.",
+    s: "...",
+    t: "-",
+    u: "..-",
+    v: "...-",
+    w: ".--",
+    x: "-..-",
+    y: "-.--",
+    z: "--..",
+};
+const morseToAlphabet = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+};
+
+const dotButton = document.getElementById("dotButton");
+const dashButton = document.getElementById("dashButton");
+const submitButton = document.getElementById("submitButton");
+
+const leterToMorse = (str) => {
+    let morse = "";
+    for (let i = 0; i < str.length; i++) {
+        morse += alphabet[str[i]];
+    }
+    return morse;
+};
+
+const morseToLetter = (morse) => {
+    if (!morseToAlphabet[morse]) {
+        return "Error";
+    } else {
+        return morseToAlphabet[morse];
+    }
+};
