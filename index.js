a237e633041266e9e0f5bac815d3a2a3a163901f
Joe Steven <stevenjoe1312@gmail.com>
2024-01-16 20:27:42 +0700

    Refactor Morse code game functionality
    Update handling of Morse code input, word validation, and displaying.
    Introduce asynchronous word retrieval. Add a delete button to correct
    input mistakes. Improve code organization. Remove redundant
    alphabet-to-Morse and Morse-to-alphabet mapping. Address issue #22.

--- index.js
+++ index.js
@@ -1,76 +1,88 @@
-const alphabetToMorse = {
-    a: ".-",
-    b: "-...",
-    c: "-.-.",
-    d: "-..",
-    e: ".",
-    f: "..-.",
-    g: "--.",
-    h: "....",
-    i: "..",
-    j: ".---",
-    k: "-.-",
-    l: ".-..",
-    m: "--",
-    n: "-.",
-    o: "---",
-    p: ".--.",
-    q: "--.-",
-    r: ".-.",
-    s: "...",
-    t: "-",
-    u: "..-",
-    v: "...-",
-    w: ".--",
-    x: "-..-",
-    y: "-.--",
-    z: "--..",
-};
-const morseToAlphabet = {
-    ".-": "a",
-    "-...": "b",
-    "-.-.": "c",
-    "-..": "d",
-    ".": "e",
-    "..-.": "f",
-    "--.": "g",
-    "....": "h",
-    "..": "i",
-    ".---": "j",
-    "-.-": "k",
-    ".-..": "l",
-    "--": "m",
-    "-.": "n",
-    "---": "o",
-    ".--.": "p",
-    "--.-": "q",
-    ".-.": "r",
-    "...": "s",
-    "-": "t",
-    "..-": "u",
-    "...-": "v",
-    ".--": "w",
-    "-..-": "x",
-    "-.--": "y",
-    "--..": "z",
-};
+import alphabetToMorse from "./alphabetToMorse.js";
+import morseToAlphabet from "./morseToAlphabet.js";
 
 const dotButton = document.getElementById("dotButton");
 const dashButton = document.getElementById("dashButton");
+const deleteButton = document.getElementById("deleteButton");
 const submitButton = document.getElementById("submitButton");
+const currMorseEl = document.getElementById("currMorse");
+const inputMorseEl = document.getElementById("inputMorse");
+const containerWordDisplay = document.querySelector(".container-wordDisplay");
 
-const leterToMorse = (str) => {
-    let morse = "";
-    for (let i = 0; i < str.length; i++) {
-        morse += alphabet[str[i]];
-    }
-    return morse;
-};
+let playing = true;
+let inputMorse = "";
+let currentMorse;
 
-const morseToLetter = (morse) => {
-    if (!morseToAlphabet[morse]) {
-        return "Error";
-    } else {
-        return morseToAlphabet[morse];
+async function init() {
+    playing = true;
+    inputMorse = "";
+    const currentWord = await getRandomWord();
+    currentMorse = wordToMorse(currentWord);
+    render(currentMorse);
+    console.log(currentMorse);
+}
+
+async function getRandomWord() {
+    const response = await fetch("https://random-word-api.herokuapp.com/word");
+    const data = await response.json();
+    return data[0];
+}
+
+function wordToMorse(str) {
+    return str
+        .split("")
+        .map((char) => alphabetToMorse[char.toLowerCase()])
+        .join(" ");
+}
+
+function morseToLetter(morse) {
+    return morse
+        .split(" ")
+        .map((code) => morseToAlphabet[code])
+        .join("");
+}
+
+function handleGuess() {
+    const correctGuess = currentMorse === inputMorse;
+    if (correctGuess) {
+        const span = document.createElement("span");
+        span.textContent = morseToLetter(currentMorse);
+        containerWordDisplay.appendChild(span);
     }
-};
+}
+
+function render(morse) {
+    const wordHtml = morse
+        .split(" ")
+        .map(() => '<span class="letter">-</span>')
+        .join("");
+    console.log(wordHtml);
+    containerWordDisplay.innerHTML = wordHtml;
+}
+
+function updateInputMorse(char) {
+    if (playing && inputMorse.length < 4) {
+        inputMorse += char;
+        currMorseEl.innerHTML = inputMorse;
+    }
+}
+
+dotButton.addEventListener("click", () => updateInputMorse("."));
+dashButton.addEventListener("click", () => updateInputMorse("-"));
+
+deleteButton.addEventListener("click", () => {
+    if (playing && inputMorse.length > 0) {
+        inputMorse = inputMorse.slice(0, -1);
+        currMorseEl.innerHTML = inputMorse;
+    }
+});
+
+submitButton.addEventListener("click", () => {
+    if (playing) {
+        const input = inputMorseEl.value;
+        playing = false;
+        handleGuess(input);
+    }
+});
+
+init();
